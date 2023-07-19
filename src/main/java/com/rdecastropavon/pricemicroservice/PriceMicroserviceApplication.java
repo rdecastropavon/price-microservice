94bfdf02c9713800d1dc34477146af84885c1bd3
rdecastropavon <rdecastropavon@gmail.com>
2023-07-19 17:31:15 +0200

    chore: add initial pom.xml and project structure

---
+++ src/main/java/com/rdecastropavon/pricemicroservice/PriceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rdecastropavon.pricemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceMicroserviceApplication.class, args);
+	}
+
+}
