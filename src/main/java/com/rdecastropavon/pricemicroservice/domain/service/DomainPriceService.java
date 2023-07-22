1ea7e83733e525ecca663b9c38773c9473eaed86
rdecastropavon <rdecastropavon@gmail.com>
2023-07-22 10:20:11 +0200

    refactor: add some logs for traceability

--- src/main/java/com/rdecastropavon/pricemicroservice/domain/service/DomainPriceService.java
+++ src/main/java/com/rdecastropavon/pricemicroservice/domain/service/DomainPriceService.java
@@ -3,15 +3,18 @@ package com.rdecastropavon.pricemicroservice.domain.service;
 import com.rdecastropavon.pricemicroservice.domain.Price;
 import com.rdecastropavon.pricemicroservice.domain.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
+@Log4j2
 public class DomainPriceService implements PriceService {
   private final PriceRepository priceRepository;
 
   @Override
   public Price findValidPrice(Long brandID, Long productID, LocalDateTime date) {
+    log.debug("findValidPrice => brandID={}, productID={}, date={}", brandID, productID, date);
     return priceRepository.findByMaxPriority(brandID, productID, date)
       .orElseThrow(() -> new PriceNotFoundException("No Price found for the selected parameters."));
   }
--- src/main/java/com/rdecastropavon/pricemicroservice/infrastructure/repository/h2/H2PriceRepository.java
+++ src/main/java/com/rdecastropavon/pricemicroservice/infrastructure/repository/h2/H2PriceRepository.java
@@ -3,6 +3,7 @@ package com.rdecastropavon.pricemicroservice.infrastructure.repository.h2;
 import com.rdecastropavon.pricemicroservice.domain.Price;
 import com.rdecastropavon.pricemicroservice.domain.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -13,16 +14,20 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class H2PriceRepository implements PriceRepository {
 
   private final SpringDataH2PriceRepository springDataH2PriceRepository;
 
   @Override
   public Optional<Price> findByMaxPriority(Long brandID, Long productID, LocalDateTime date) {
+    log.debug("findByMaxPriority => brandID={}, productID={}, date={}", brandID, productID, date);
     Page<PriceEntity> pricePage = springDataH2PriceRepository.findByMaxPriority(brandID, productID, date,
       PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "priority")));
 
+    log.debug("findByMaxPriority pricePage => pricePage={}", pricePage);
     Optional<PriceEntity> price = pricePage.stream().findFirst();
+    log.debug("findByMaxPriority price => price={}", price);
 
     return price.stream().map(PriceEntity::toPrice).findFirst();
   }
