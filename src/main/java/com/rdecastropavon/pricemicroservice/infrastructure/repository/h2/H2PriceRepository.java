e8cc7be6c7428ae9776234e55b6aaf5d1a4bee78
rdecastropavon <rdecastropavon@gmail.com>
2023-07-27 10:44:28 +0200

    perf: change findByMaxPriority query to use native and avoid count(*)

--- src/main/java/com/rdecastropavon/pricemicroservice/infrastructure/repository/h2/H2PriceRepository.java
+++ src/main/java/com/rdecastropavon/pricemicroservice/infrastructure/repository/h2/H2PriceRepository.java
@@ -4,9 +4,6 @@ import com.rdecastropavon.pricemicroservice.domain.Price;
 import com.rdecastropavon.pricemicroservice.domain.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -22,13 +19,10 @@ public class H2PriceRepository implements PriceRepository {
   @Override
   public Optional<Price> findByMaxPriority(Long brandID, Long productID, LocalDateTime date) {
     log.debug("findByMaxPriority => brandID={}, productID={}, date={}", brandID, productID, date);
-    Page<PriceEntity> pricePage = springDataH2PriceRepository.findByMaxPriority(brandID, productID, date,
-      PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "priority")));
 
-    log.debug("findByMaxPriority pricePage => pricePage={}", pricePage);
-    Optional<PriceEntity> price = pricePage.stream().findFirst();
-    log.debug("findByMaxPriority price => price={}", price);
+    Optional<PriceEntity> priceEntity = springDataH2PriceRepository.findByMaxPriority(brandID, productID, date);
+    log.debug("findByMaxPriority priceEntity => priceEntity={}", priceEntity);
 
-    return price.stream().map(PriceEntity::toPrice).findFirst();
+    return priceEntity.stream().map(PriceEntity::toPrice).findFirst();
   }
 }
--- src/main/java/com/rdecastropavon/pricemicroservice/infrastructure/repository/h2/SpringDataH2PriceRepository.java
+++ src/main/java/com/rdecastropavon/pricemicroservice/infrastructure/repository/h2/SpringDataH2PriceRepository.java
@@ -1,19 +1,19 @@
 package com.rdecastropavon.pricemicroservice.infrastructure.repository.h2;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface SpringDataH2PriceRepository extends JpaRepository<PriceEntity, PriceEntityID> {
   
-  @Query("SELECT E FROM #{#entityName} E "
-    + "WHERE E.brand.id=:brandID AND E.productID=:productID AND :date BETWEEN E.startDate" + " AND " + "E.endDate ")
-  Page<PriceEntity> findByMaxPriority(@Param("brandID") Long brandID, @Param("productID") Long productID,
-    @Param("date") LocalDateTime date, Pageable pageable);
+  @Query(value =
+    "SELECT * FROM PRICES P WHERE P.brand_id=:brandID AND P.product_ID=:productID AND :date BETWEEN P.START_DATE "
+      + "AND P.END_DATE ORDER BY P.PRIORITY DESC FETCH FIRST" + " 1 ROWS ONLY", nativeQuery = true)
+  Optional<PriceEntity> findByMaxPriority(@Param("brandID") Long brandID, @Param("productID") Long productID,
+    @Param("date") LocalDateTime date);
 }
